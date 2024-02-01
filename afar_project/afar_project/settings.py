21828e082c37efca6f7bd8eea01f03c7a240a803
Anik Kumar Mallick <119910227+ANIKFRCBD@users.noreply.github.com>
2024-02-01 16:10:43 +0600

    Update settings.py

--- afar_project/afar_project/settings.py
+++ afar_project/afar_project/settings.py
@@ -38,13 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'afar_app',
-<<<<<<< HEAD
     'dashboard',
     'users',
     'impairmenttest',
-=======
-    'dashboard'
->>>>>>> c66c41def6816c09e1eddc74a3d211422267f724
 ]
 
 MIDDLEWARE = [
